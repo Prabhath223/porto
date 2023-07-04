87f4b0fa5f83bec12b877d4f7f24e86eeda80e2f
Prabhath223 <nagulaprabhath@gmail.com>
2023-07-04 22:39:53 +0530

    Your commit message here

--- src/MyComps/Services.js
+++ src/MyComps/Services.js
@@ -3,106 +3,25 @@ import React from "react";
 function MyServices() {
   return (
     <section className="section service" id="service" aria-label="service">
-      <div className="container">
-        <h2 className="h2 section-title">Services We Provided</h2>
+      <div className="cntainer">
+       <div className="bx1">dff<br></br>
+       dff<br></br>
+       
+       dff<br></br>
+       dff<br></br>
+       dff<br></br>
+       dff<br></br>
+       dff<br></br>
+       dff<br></br>
+       dff<br></br></div>
+      
 
-        <p className="section-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
-        </p>
+       <div className="bx2">dr</div>
+       <div className="bx3">de</div>
+       </div>
+    
 
-        <ul className="grid-list">
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#13c4a1" }}>
-                <ion-icon name="chatbox"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">SEO Optimization</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#6610f2" }}>
-                <ion-icon name="desktop"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">Digital Marketing</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#ffb700" }}>
-                <ion-icon name="bulb"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">Market Research</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#fc3549" }}>
-                <ion-icon name="phone-portrait"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">Keyword Targeting</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#00d280" }}>
-                <ion-icon name="archive"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">Email Marketing</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <div className="service-card">
-              <div className="card-icon" style={{ backgroundColor: "#ff612f" }}>
-                <ion-icon name="build"></ion-icon>
-              </div>
-              <h3 className="h3">
-                <a href="#" className="card-title">Marketing & Reporting</a>
-              </h3>
-              <p className="card-text">
-                Sed ut perspiciatis unde omnis iste natus error sit tatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-              </p>
-          
-            </div>
-          </li>
-        </ul>
-        <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-        <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-        <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-        <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-        <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-                <p>dhsjdsdjh</p>  <br></br><p>dhsjdsdjh</p>  <p>dhsjdsdjh</p>
-      </div>
+      
     </section>
   );
 }
